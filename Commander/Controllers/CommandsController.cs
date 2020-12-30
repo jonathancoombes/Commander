ea61107a37fb8cecb78aac0cb7240f9d6cfe6a05
jonathancoombes <coombes@web.de>
2020-12-31 00:03:56 +0200

    Add project files.

---
+++ Commander/Controllers/CommandsController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Commander.Data;
+using Commander.Models;
+
+namespace Commander.Controllers
+{
+    [Route("api/commands")]
+    [ApiController]
+    public class CommandsController : ControllerBase
+
+    {
+
+        private readonly ICommanderRepo _repositoryRepo;
+
+        public CommandsController(ICommanderRepo commanderRepo)
+        {
+            _repositoryRepo = commanderRepo;
+
+        }
+
+        //GET Example: api/commands
+        [HttpGet]
+            public ActionResult <IEnumerable<Command>>GetAllCommands()
+            {
+
+                var commandItems =  _repositoryRepo.GetAppCommands();
+
+                return Ok(commandItems);
+
+
+            }
+            //GET Example: api/commands/5
+            [HttpGet("{Id}")]
+            public ActionResult<Command> GetCommandById(int Id)
+            {
+                var commandItem = _repositoryRepo.GetCommandById(Id);
+
+                return Ok(commandItem);
+
+
+            }
+
+    }
+}
---
+++ Commander/Data/MockCommanderRepo.cs
@@ -0,0 +1,29 @@
+﻿using Commander.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration.CommandLine;
+
+namespace Commander.Data
+{
+    public class MockCommanderRepo : ICommanderRepo
+    {
+        public IEnumerable<Command> GetAppCommands()
+        {
+            var commands = new List<Command>
+            {
+                new Command {HowTo = "Run DotNet", Id = 0, Line = "dotnet run", Platform = "DotNet"},
+                new Command {HowTo = "Autorun DotNet Project", Id = 1, Line = "dotnet watch run", Platform = "DotNet"},
+                new Command {HowTo = "Build DotNet Project", Id = 2, Line = "dotnet build", Platform = "DotNet"}
+            };
+
+            return commands;
+        }
+
+        public Command GetCommandById(int Id)
+        {
+           return new Command {HowTo = "Create new DotNet Project", Id = 0, Line = "dotnet new", Platform = "DotNet"};
+        }
+    }
+}
