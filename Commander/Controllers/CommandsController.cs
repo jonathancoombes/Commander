603f146aafe38afcc378ea8c3c5cf4883ec81277
jonathancoombes <coombes@web.de>
2021-01-03 22:48:36 +0200

    Added Sql Repo

--- Commander/Controllers/CommandsController.cs
+++ Commander/Controllers/CommandsController.cs
@@ -3,8 +3,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using Commander.Data;
 using Commander.Models;
+using Commander.Dtos;
 
 namespace Commander.Controllers
 {
@@ -15,33 +17,43 @@ namespace Commander.Controllers
     {
 
         private readonly ICommanderRepo _repositoryRepo;
+        private readonly IMapper _mapper;
 
-        public CommandsController(ICommanderRepo commanderRepo)
+        public CommandsController(ICommanderRepo commanderRepo, IMapper mapper)
         {
             _repositoryRepo = commanderRepo;
+            _mapper = mapper;
 
         }
 
         //GET Example: api/commands
         [HttpGet]
-            public ActionResult <IEnumerable<Command>>GetAllCommands()
+            public ActionResult <IEnumerable<CommandReadDto>>GetAllCommands()
             {
 
                 var commandItems =  _repositoryRepo.GetAppCommands();
-
-                return Ok(commandItems);
+                if(commandItems != null)
+                {
+                    return Ok(_mapper.Map<IEnumerable<CommandReadDto>>(commandItems));
+                }
+                else
+                {
+                    return NoContent();
+                }
 
 
             }
-            //GET Example: api/commands/5
+            //GET Example: api/commands/{Id}}
             [HttpGet("{Id}")]
-            public ActionResult<Command> GetCommandById(int Id)
+            public ActionResult<CommandReadDto> GetCommandById(int Id)
             {
                 var commandItem = _repositoryRepo.GetCommandById(Id);
+                if (commandItem != null)
+                {
+                    return Ok(_mapper.Map<CommandReadDto>(commandItem));
+                }
 
-                return Ok(commandItem);
-
-
+                return NotFound();
             }
 
     }
---
+++ Commander/Data/SqlCommanderRepo.cs
@@ -0,0 +1,40 @@
+﻿using Commander.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Commander.Data
+{
+    public class SqlCommanderRepo : ICommanderRepo
+    {
+        private readonly IMapper _mapper;
+        private readonly CommanderContext _context;
+
+        public SqlCommanderRepo(IMapper mapper, CommanderContext context)
+        {
+            _mapper = mapper;
+            _context = context;
+        }
+
+
+        public IEnumerable<Command> GetAppCommands()
+        {
+
+            var result = _context.Commands.ToList();
+
+            return result;
+
+        }
+
+        Command ICommanderRepo.GetCommandById(int id)
+        {
+            var result = _context.Commands.SingleOrDefault(p => p.Id == id);
+          
+                return result;
+            
+        }
+    }
+}
